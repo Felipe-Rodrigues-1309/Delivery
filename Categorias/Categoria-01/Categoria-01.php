5d164ded03b9f674e08d2888ceb1c7ca77be44d9
Felipe <feliphi10@gmail.com>
2026-01-05 23:59:49 -0300

    removido modal

--- Categorias/Categoria-01/Categoria-01.php
+++ Categorias/Categoria-01/Categoria-01.php
@@ -76,86 +76,13 @@ $result = $conn->query($sql);
       <h3><?php echo $row['item']; ?></h3>
       <p>Cod: <?php echo $row['cod']; ?></p>
       <p>Valor R$ <?php echo number_format($row['valor'],2,",","."); ?></p>
-      <button class="butao" data-bs-toggle="modal" data-bs-target="#modal<?php echo $row['id']; ?>">
-        Comprar
+      <button class="butao btn-add-carrinho"
+        data-id="<?php echo $row['id']; ?>"
+        data-nome="<?php echo htmlspecialchars($row['item'], ENT_QUOTES); ?>"
+        data-total="<?php echo $row['valor']; ?>">
+        Adicionar ao Carrinho
       </button>
     </div>
-   <!--inicio conteudo modal-->
-    <div class="modal fade" id="modal<?php echo $row['id']; ?>" tabindex="-1" aria-labelledby="modalProduto1" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-10" id="modalProduto1"><?php echo $row['item'];?></h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-          <span>Descição</span><br>
-          <span><?php echo $row['descricao'];?></span>  <!--pegar a descrição do banco-->
-          <hr>
-          <!---incio adicionais -->
-          
-          <label for="Adicionais">Adicionais ?</label><br>
-          
-          <?php
-          // Array para mapear as colunas de adicionais
-          $adicionais_map = [
-              'adicional1' => 'valoradicional1',
-              'adicional2' => 'valoradicional2', // Assumindo que existem mais colunas
-              'adicional3' => 'valoradicional3',
-              'adicional4' => 'valoradicional4',
-              'adicional5' => 'valoradicional5',
-              'adicional6' => 'valoradicional6',
-              'adicional7' => 'valoradicional7',
-              'adicional8' => 'valoradicional8',
-              'adicional9' => 'valoradicional9',
-              'adicional10' => 'valoradicionall0', // Assumindo que existem mais colunas
-              // Adicione mais pares de colunas conforme a estrutura da sua tabela 'produtos'
-          ];
-
-          $produto_id = $row['id'];
-          $tem_adicional = false;
-
-          foreach ($adicionais_map as $nome_coluna => $valor_coluna) {
-              // Verifica se o nome do adicional e o valor existem e não estão vazios
-              if (!empty($row[$nome_coluna]) && isset($row[$valor_coluna])) {
-                  $nome_adicional = $row[$nome_coluna];
-                  $valor_adicional = $row[$valor_coluna];
-                  $tem_adicional = true;
-          ?>
-                  <input 
-                      type="checkbox" 
-                      name="adicional_<?php echo $produto_id; ?>[]" 
-                      id="adicional_<?php echo $nome_coluna; ?>_<?php echo $produto_id; ?>" 
-                      data-valor="<?php echo $valor_adicional; ?>"
-                      class="adicional-checkbox"
-                      data-produto-id="<?php echo $produto_id; ?>"
-                      value="<?php echo $nome_adicional; ?>"
-                  >
-                  <label for="adicional_<?php echo $nome_coluna; ?>_<?php echo $produto_id; ?>">
-                      <?php echo $nome_adicional; ?> (+R$ <?php echo number_format($valor_adicional, 2, ",", "."); ?>)
-                  </label><br>
-          <?php
-              }
-          }
-
-          if (!$tem_adicional) {
-              echo "<p>Nenhum adicional disponível para este produto.</p>";
-          }
-          ?>
-                <h2 id = "valorTotal<?php echo $row['id']; ?>" data-valor-base="<?php echo $row['valor'];?>">Total: R$ <?php echo number_format($row['valor'],2,",","."); ?></h2>
-                <!---final adicionais -->
-
-        </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary btn-add-carrinho"
-            data-id = "<?php echo $row['id']; ?>"
-            data-nome="<?php echo htmlspecialchars($row['item'], ENT_QUOTES); ?>">Adicionar ao Carrinho</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!--final conteudo modal -->
     <?php endwhile; ?>
   </div>
 </div> 
