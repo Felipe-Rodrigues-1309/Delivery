453da7608fbd0015a61c76c954c936d14155ff38
Felipe Rodrigues <feliphi10@gmail.com>
2026-03-22 02:24:39 -0300

    teste

--- app/controllers/cadastroDeCliente.php
+++ app/controllers/cadastroDeCliente.php
@@ -18,9 +18,7 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 }
 
 // criptografa senha
-$senhaHash =
-
-password_hash($senha, PASSWORD_DEFAULT);
+$senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
 // verifica se o e-mail já existe
 $sqlCheck = "SELECT id FROM usuarios WHERE email = ?";
